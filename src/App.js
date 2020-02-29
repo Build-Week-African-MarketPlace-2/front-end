38b3397501e8a269e91a6303fd8ba93bcf6e887a
Logan Van Hook <loganvan@att.net>
2020-02-28 21:29:29 -0500

    Initialize

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+function App()
+{
+  return (
+    <HeadStyle>
+      
+    </HeadStyle>
+  );
+}
+
+let HeadStyle = styled.div`
+  
+`;
+
+export default App;
