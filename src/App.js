c59a39c64ab8015e03df458c692672c12e2212d1
Logan Van Hook <loganvan@att.net>
2020-03-03 12:03:30 -0500

    CORS invalid HTTPS

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App()
 
   let GetListings = function()
   {
-    Axios.get("https://158.69.205.136/nothing").then((response)=>
+    Axios.get("https://loganv.codes/nothing").then((response)=>
     {
       sListings([
         {
